5d2b40bc6d1c9e6d6db3d8d072fdd86020ee5413
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-01-26 18:00:51 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//패키지 아래에 있으면 안됨.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
