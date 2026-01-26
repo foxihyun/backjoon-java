27b45671ac1d3eb04cbf1d6a32e857b746d451af
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-01-26 18:23:42 +0900

    solve BOJ 2557,1000

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-//패키지 아래에 있으면 안됨.
+//백준 제출할 때 패키지 아래에 있으면 안됨.
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Scanner sc = new Scanner(System.in);
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+       System.out.println(x+y);
     }
 }
---
+++ src/bronze/B1000.java
@@ -0,0 +1,16 @@
+package bronze;
+
+//백준 제출할 때 패키지 아래에 있으면 안됨.
+
+import java.io.*;
+import java.util.*;
+
+public class B1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x+y);
+    }
+}
+
