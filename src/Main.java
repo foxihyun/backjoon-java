c6f83e90c230a9c56ca7d99506724f61d32de2ec
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-01-26 18:55:16 +0900

    solve BOJ 2438

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//백준 제출할 때 패키지 아래에 있으면 안됨.
-
-import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-      int x = sc.nextInt();
-      int y = sc.nextInt();
-       System.out.println(x+y);
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
