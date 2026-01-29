8cb7873bab1c9d97f3b7ed60576827f5c735eb4b
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-01-29 17:14:13 +0900

    solve BOJ 10809,2675

--- src/bronze/B10809.java
+++ src/bronze/B10809.java
@@ -1,4 +1,34 @@
 package bronze;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class B10809 {
+    public static void main(String[] args) {
+        // 문자열 입력 -> a부터 z까지 모두 출력해주되 알파벳이 나오면 나오는 처음 위치, 없으면 -1 출력
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine(); // 문자열 입력 받음
+        //int[] Out = {-1, -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //초기화
+        //인덱스 찾기 charat 이용해서 그 위치에 넣어주기?
+        //예외는 중복일때?
+        //질문-> 그러면 int[] out = new int[23]; 이랑 저렇게 직접 넣어주는 것이랑 뭐가 다른지?
+        int[] Out = new int[26];
+        Arrays.fill(Out,-1); // 배열을 -1로 채우는 함수
+        int count = 0;
+        for(int i =0;i<s.length();i++)
+        {
+            int A = s.charAt(i)-'a'; //s[i]가 아니라 s.charAt[i]로 사용해야함!!
+            // 예를들어 b다. s[0] -'a' -> 1 임. 그러면Our[1]= 0 이런 식으로 차곡 차곡 넣어주면됨.
+            if(Out[A]== -1) //첫 등장일 경우에만 -> Out[A] == -1 일때만
+            {
+
+                Out[A] = i;
+            }
+        }
+
+        for(int j=0;j<Out.length;j++)
+        {
+            System.out.printf("%d ",Out[j]);
+        }
+    }
 }
--- src/bronze/B2675.java
+++ src/bronze/B2675.java
@@ -1,4 +1,30 @@
 package bronze;
 
+import java.util.Scanner;
+
 public class B2675 {
+    public static void main(String[] args) {
+        //T 입력. (테스트 횟수);
+        //R 입력. (반복 횟수)
+        //문자열 입력 -> 반복 횟수 합쳐서 출력
+        //charat[i] * R 출력
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int count = 0;
+        while(count<T)
+        {
+            int R = sc.nextInt();
+            String S = sc.next(); //NEXT와 NEXTLINE의 차이점?
+
+            for(int j=0;j<S.length();j++) { //출력 부분
+                for (int i = 0; i < R; i++) {
+                    System.out.printf("%s", S.charAt(j));
+                }
+            }
+            System.out.println(); // 줄바꿈
+            count++;
+        }
+
+    }
 }
