cc6a352e1bdbb4fd88b7fbeb48e2cc52d1f0a5f2
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-01-29 18:54:10 +0900

    solve BOJ 8958

--- src/bronze/B8958.java
+++ src/bronze/B8958.java
@@ -1,6 +1,8 @@
-import java.util.*;
+package bronze;
 
-public class Main {
+import java.util.Scanner;
+
+public class B8958 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
