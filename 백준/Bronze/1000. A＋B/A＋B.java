b19a6d26851da819038367db7d066727573c30ed
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-03-29 19:13:10 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+//백준 제출할 때 패키지 아래에 있으면 안됨.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+       System.out.println(x+y);
+    }
+}
