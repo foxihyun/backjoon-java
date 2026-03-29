e60a65a39885cc7412cafaaf98bd96d31ef8067a
박시현 <113010940+foxihyun@users.noreply.github.com>
2026-03-29 19:12:42 +0900

    [Bronze IV] Title: 숫자의 합, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String num = sc.next();
+
+        int sum=0;
+        for(int i=0;i<num.length();i++)
+        {
+            int RealNum= num.charAt(i)-'0';
+            sum += RealNum;
+        }
+        System.out.println(sum);
+    }
+}
